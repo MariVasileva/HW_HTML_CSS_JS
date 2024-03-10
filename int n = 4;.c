190fe933ca2493a484d25e47f89fcf019e0456ea
Mari Vasileva <marivasileva90@gmail.com>
2024-03-10 17:26:16 +0500

    HW

---
+++ int n = 4;.c
@@ -0,0 +1,7 @@
+int n = 4;
+	int age[n];
+	for (int i = 0; i < n; i++)
+	{	
+		printf("Возраст человека #%i : ", i+1);
+		age[i]=GetInt();
+	}
---
+++ test.py
@@ -0,0 +1,10 @@
+#print('что-то')
+#x = input('eer: ')
+#print(x)
+
+
+my_list = ["Python", "JavaScript", "C++", "Java", "C#", "Dart"]
+print(my_list[-1])  
+print(my_list[-2]) 
+
+print("rd")
